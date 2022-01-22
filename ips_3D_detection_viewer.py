fb9b94ed6f86a779c9aedf08740877cce0fd644b
cliu <chenghua.liu@ika.rwth-aachen.de>
2022-01-22 02:29:25 +0100

    添加控件

--- ips_3D_detection_viewer.py
+++ ips_3D_detection_viewer.py
@@ -25,18 +25,31 @@ def ips_viewer(ipu,cam,lidar_com):
         vi.add_points(points[:,:3],scatter_filed=points[:,2],color_map_name='viridis')#原始lidar点，添加到2d和3d场景队列。不涉及任何转换
         vi.add_3D_boxes(labels,box_info=label_names)#转换bounding box到vtk格式的线段和顶点list,问题出在读取label时候xyz就不对了，angle和whl都对，我知道了，按double读取int就导致xyz错误了！！！
         #vi.add_3D_cars(labels, box_info=label_names)#只是为了添加3d车辆，没什么用
-        vi.add_image(image,deep_copy=True)#只是添加图片，没什么看的
+
+        vi.set_intrinsic_mat(P2)#设置内参，没什么问题
+
+        if True:    #设置面板
+            import cv2
+            cv2.namedWindow('Setting matrix')
+            #定义回调函数
+            def nothing(x):
+                pass
+            cv2.createTrackbar('threshold1','Setting matrix',50,400,nothing)
+            cv2.createTrackbar('threshold2','Setting matrix',100,400,nothing)
+            while(1):
+                #返回滑动条所在位置的值
+                threshold1=cv2.getTrackbarPos('threshold1','Setting matrix')
+                threshold2=cv2.getTrackbarPos('threshold2','Setting matrix')
+                #Canny边缘检测
+                if cv2.waitKey(1)==ord('q'):
+                    break
+
         #从这里截断，循环设置外参
         while(True):
-            V2C[0,3]+=i
+            vi.add_image(image,deep_copy=True)#只是添加图片，没什么看的
             vi.set_extrinsic_mat(V2C)#设置外参，也没什么看的
-            vi.set_intrinsic_mat(P2)#设置内参，没什么问题
-            
-            vi.show_2D()
-            vi.image=image
 
-            print(i)
-            i+=1
+            #vi.show_2D()
             #vi.show_3D()
 
 
